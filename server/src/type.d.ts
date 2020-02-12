74207cb5cd406035c6455355bf82b892d833d6a4
Allen Elguira <AllenElguira16@gmail.com>
2020-02-12 22:43:07 +0800

    Revert "Migrate to NextJS"
    This reverts commit ec4f064668dc191180449f3d9f68506fc64e6d06.

---
+++ server/src/type.d.ts
@@ -0,0 +1,55 @@
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// export interface Admin {
+//   username: string;
+//   password: string;
+// }
+
+export interface User {
+  id: string
+  schoolID: string
+  type: 'Employee' | 'Student'
+  firstname: string
+  lastname: string
+  dateCreated: Date
+}
+
+export type UserInput = Omit<User, 'id' | 'dateCreated'>
+
+export type AdminInput = {
+  _id: Partial<string>
+  username: string
+  password: string
+}
+
+export interface Setting {
+  name: string
+  value: any
+}
+
+export interface Response {
+  success?: any
+  error?: any
+}
+
+export interface SessionInterface {
+  user: AdminInput
+}
+// export interface UserInterface {
+//   userId: string;
+//   firstname: string;
+//   lastname: string;
+// }
+
+// export interface VehicleInterface {
+//   userId: string;
+//   name: string;
+//   plateNumber: string;
+//   type: string;
+//   color: string;
+//   registrationNumber: string;
+// }
+
+// export interface AdminParams {
+//   username: string;
+//   password: string;
+// }
